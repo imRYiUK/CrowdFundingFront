739f8ab2ee0a90564da25b065157f6d5c68ab314
imryuik <imahmedtidiane@gmail.com>
2023-12-04 16:49:42 +0000

    change on login and register

---
+++ src/app/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id ?: number,
+  name: string,
+  surname: string,
+  phone_number: string,
+  email: string,
+  username: string,
+  password: string
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./components/auth/login/login.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {RegisterComponent} from "./components/auth/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '404_not_found', component: NotFoundComponent},
+  { path: '**', redirectTo: '404_not_found', pathMatch: 'full' },
+];
